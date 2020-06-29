6c54d7790c04a1f18d3090525cc44e288c07129b
dmarc <dmarcr1997@gmail.com>
2020-06-28 19:43:06 -0500

    created path from user show page to new file page and user files page

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,2 +1,22 @@
-class UserFilesController < ApplicationController
+class FoldersController < ApplicationController
+    def new
+        raise params
+        binding.pry
+    end
+
+    def index
+        raise params
+    end
+
+    def show
+        raise params
+    end
+
+    def create
+        raise params
+    end
+
+    def destroy
+        raise params
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'sessions#new'
-  resources :folders
-  resources :users
+ 
+  resources :users do
+    resources :folders
+  end
   resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
