0c398fea29af8fbc0f1a8f358fcd0a2d0fb6cace
dmarc <dmarcr1997@gmail.com>
2020-06-28 20:17:36 -0500

    started creation of files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_010027) do
+ActiveRecord::Schema.define(version: 2020_06_29_011215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2020_06_29_010027) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "user_files", force: :cascade do |t|
+    t.string "name"
+    t.string "file_data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
