755e44cdb6f4cbbf53fb92b3b8f909b3d6d25939
dmarc <dmarcr1997@gmail.com>
2020-06-28 19:14:14 -0500

    created signin and signup page

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get '/', to: 'sessions#new'
+  resources :folders
+  resources :users
+  resources :sessions
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
